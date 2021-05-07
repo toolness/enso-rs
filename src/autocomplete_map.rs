9a6f2c3bc4f62005549304a1e424f20ee7550f89
Atul Varma <varmaa@gmail.com>
2021-05-07 17:06:04 -0400

    Actually filter input.

--- src/autocomplete_map.rs
+++ src/autocomplete_map.rs
@@ -8,6 +8,44 @@ pub struct AutocompleteSuggestion<T: Clone> {
     value: T,
 }
 
+fn get_matches(input: &str, name: &str) -> Vec<Range<usize>> {
+    let mut matches = vec![];
+
+    if name.len() == 0 {
+        return matches;
+    }
+
+    let mut name_idx = 0;
+    let mut curr_match: Option<Range<usize>> = None;
+    let mut input_chars = input.chars();
+    let mut input_char = input_chars.next().unwrap();
+
+    for name_char in name.chars() {
+        if name_char == input_char {
+            curr_match = match curr_match {
+                None => Some(name_idx..name_idx + 1),
+                Some(range) => Some(range.start..name_idx + 1),
+            };
+            if let Some(next_input_char) = input_chars.next() {
+                input_char = next_input_char;
+            } else {
+                break;
+            }
+        } else if let Some(match_) = curr_match {
+            matches.push(match_);
+            curr_match = None;
+        }
+
+        name_idx += 1;
+    }
+
+    if let Some(match_) = curr_match {
+        matches.push(match_);
+    }
+
+    matches
+}
+
 pub struct AutocompleteMap<T: Clone> {
     entries: HashMap<String, T>,
 }
@@ -29,14 +67,17 @@ impl<T: Clone> AutocompleteMap<T> {
         max_results: usize,
     ) -> Vec<AutocompleteSuggestion<T>> {
         let mut results = Vec::with_capacity(max_results);
+        let input_string = input.into();
 
-        // TODO: Actually filter to input.
         for (name, value) in self.entries.iter() {
-            results.push(AutocompleteSuggestion {
-                name: name.clone(),
-                matches: vec![0..2],
-                value: value.clone(),
-            });
+            let matches = get_matches(input_string.as_str(), name.as_str());
+            if !matches.is_empty() {
+                results.push(AutocompleteSuggestion {
+                    name: name.clone(),
+                    matches,
+                    value: value.clone(),
+                })
+            }
         }
 
         results
@@ -60,6 +101,7 @@ mod tests {
     fn test_it_works() {
         let mut am = AutocompleteMap::new();
         am.insert("boop", 1);
+        am.insert("goop", 1);
         assert_eq!(am.autocomplete("bo", 1), vec![sugg("boop", vec![0..2], 1)]);
     }
 }
