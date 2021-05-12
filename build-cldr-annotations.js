a4d23f1a83db7d76edc0358f46ae992e99e9aac8
Atul Varma <varmaa@gmail.com>
2021-05-12 06:36:41 -0400

    Make 'insert' commands ascii. Works around #10.

--- build-cldr-annotations.js
+++ build-cldr-annotations.js
@@ -1,3 +1,5 @@
+//@ts-check
+
 const fs = require('fs');
 const path = require('path');
 
@@ -8,6 +10,47 @@ if (!fs.existsSync(JSON_FILENAME)) {
   console.log(`Please download the JSON version of the Unicode CLDR annotations and save it in the current directory as ${JSON_FILENAME}.`);
   console.log('You can download it from: https://raw.githubusercontent.com/unicode-org/cldr-json/master/cldr-json/cldr-annotations-full/annotations/en/annotations.json');
   process.exit(1);
+}
+
+const unknownChars = new Set();
+
+/**
+ * @arg count {number}
+ * @returns {String}
+ */
+function spaces(count) {
+  let spaces = [];
+
+  for (let i = 0; i < count; i++) {
+    spaces.push(' ');
+  }
+
+  return spaces.join('');
+}
+
+/**
+ * @arg value {String}
+ * @returns {String}
+ */
+function formatForCommandName(value) {
+  value = value
+    .toLocaleLowerCase()
+    .replace(/[“”]/g, '"')
+    .replace(/’/g, "'")
+    .replace(/ñ/g, "n");
+
+  for (let i = 0; i < value.length; i++) {
+    let codePoint = value.codePointAt(i);
+    if (codePoint < 32 || codePoint > 126) {
+      if (!unknownChars.has(codePoint)) {
+        unknownChars.add(codePoint);
+        console.warn(`WARNING: Unicode codepoint ${codePoint} is not currently supported by Enso:`);
+        console.warn(`  ${value}`);
+        console.warn(`  ${spaces(i)}^\n`);
+      }
+    }
+  }
+  return value;
 }
 
 const data = JSON.parse(fs.readFileSync('annotations.json', { encoding: 'utf-8' }));
@@ -23,8 +66,9 @@ const lines = [
   `pub const CLDR_ANNOTATIONS: [(&'static str, &'static str); ${numEntries}] = [`
 ];
 
-for ([char, item] of Object.entries(data.annotations.annotations)) {
-  lines.push(`  (${JSON.stringify(char)}, "${item.tts[0]}"),`);
+for (let [char, item] of Object.entries(data.annotations.annotations)) {
+  const name = formatForCommandName(item.tts[0]);
+  lines.push(`  (${JSON.stringify(char)}, "${name}"),`);
 }
 
 lines.push('];\n');
