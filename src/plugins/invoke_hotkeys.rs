173ad7712072a45a9e4209e157ef9d8593a82276
Atul Varma <varmaa@gmail.com>
2023-03-18 19:04:44 -0400

    Support multiple apps for hotkeys.

--- src/plugins/invoke_hotkeys.rs
+++ src/plugins/invoke_hotkeys.rs
@@ -42,7 +42,8 @@ impl TryFrom<&str> for HotkeyCombination {
 #[derive(Default)]
 pub struct InvokeHotkeysPlugin {
     commands_loaded: Vec<String>,
-    last_update: Option<SystemTime>,
+    last_foreground_executable_path: Option<String>,
+    last_parse: Option<(SystemTime, HotkeyParseResult)>,
 }
 
 impl InvokeHotkeysPlugin {
@@ -58,6 +59,7 @@ impl InvokeHotkeysPlugin {
     }
 
     fn parse_hotkeys(&self, hotkeys_path: PathBuf) -> Result<HotkeyParseResult, Error> {
+        println!("Parsing hotkeys from \"{}\".", hotkeys_path.display());
         let mut line_no = 0;
         let mut result = HotkeyParseResult {
             warnings: vec![],
@@ -146,24 +148,37 @@ impl InvokeHotkeysPlugin {
         Ok(result)
     }
 
-    pub fn reload(&mut self, ui: &mut UserInterface, hotkeys_path: PathBuf) -> Result<(), Error> {
-        println!("Parsing hotkeys from \"{}\".", hotkeys_path.display());
-
-        let result = self.parse_hotkeys(hotkeys_path)?;
-
-        if result.warnings.len() > 0 {
-            let message = format!(
-                "Problems occurred while parsing hotkeys file:\n{}",
-                result.warnings.join("\n")
-            );
-            eprintln!("{}", message);
-            ui.show_message(message)?;
+    fn show_parse_warnings(&self, ui: &mut UserInterface) -> Result<(), Error> {
+        if let Some((_, parse_result)) = &self.last_parse {
+            if parse_result.warnings.len() > 0 {
+                let message = format!(
+                    "Problems occurred while parsing hotkeys file:\n{}",
+                    parse_result.warnings.join("\n")
+                );
+                eprintln!("{}", message);
+                ui.show_message(message)?;
+            }
         }
+        Ok(())
+    }
 
+    fn reload(&mut self, ui: &mut UserInterface) -> Result<(), Error> {
         self.unload(ui)?;
-        for section in result.sections {
+        let Some((_, parse_result)) = self.last_parse.as_ref() else {
+            return Ok(());
+        };
+        for section in &parse_result.sections {
             let _ = section.name; // TODO: We should use this in the help text.
-            for command in section.commands {
+            if let Some((exe_filter, exe_path)) = section
+                .exe_filter
+                .as_ref()
+                .zip(self.last_foreground_executable_path.as_ref())
+            {
+                if !exe_path.contains(exe_filter) {
+                    continue;
+                }
+            }
+            for command in section.commands.clone() {
                 let hotkey = command.hotkey;
                 if ui.has_command(&command.name) {
                     println!("Command \"{}\" already exists, skipping.", command.name);
@@ -180,32 +195,51 @@ impl InvokeHotkeysPlugin {
     }
 
     pub fn maybe_reload(&mut self, ui: &mut UserInterface) -> Result<(), Error> {
+        let foreground_executable_path: Option<String> = get_foreground_executable_path().ok();
+        let did_foreground_executable_change =
+            foreground_executable_path != self.last_foreground_executable_path;
+        let was_reparsed = self.maybe_reparse()?;
+
+        if !did_foreground_executable_change && !was_reparsed {
+            Ok(())
+        } else {
+            if did_foreground_executable_change {
+                self.last_foreground_executable_path = foreground_executable_path;
+            }
+            if was_reparsed {
+                self.show_parse_warnings(ui)?;
+            }
+            self.reload(ui)
+        }
+    }
+
+    pub fn maybe_reparse(&mut self) -> Result<bool, Error> {
         let mut hotkeys_path = get_enso_home_dir()?;
         hotkeys_path.push("hotkeys.txt");
         if hotkeys_path.exists() {
             let metadata = std::fs::metadata(&hotkeys_path)?;
             let new_last_update = metadata.modified()?;
-            if let Some(last_update) = self.last_update {
+            if let Some((last_update, _)) = self.last_parse {
                 if new_last_update > last_update {
                     // The hotkeys file has changed, so reload it.
-                    self.last_update = Some(new_last_update);
-                    self.reload(ui, hotkeys_path)
+                    self.last_parse = Some((new_last_update, self.parse_hotkeys(hotkeys_path)?));
+                    Ok(true)
                 } else {
                     // The hotkeys file hasn't changed, so do nothing.
-                    Ok(())
+                    Ok(false)
                 }
             } else {
                 // We haven't loaded hotkeys before, so load them now.
-                self.last_update = Some(new_last_update);
-                self.reload(ui, hotkeys_path)
+                self.last_parse = Some((new_last_update, self.parse_hotkeys(hotkeys_path)?));
+                Ok(true)
             }
-        } else if self.last_update.is_some() {
+        } else if self.last_parse.is_some() {
             // We've loaded hotkeys before, but now the hotkeys.txt file is gone, so unload them.
-            self.last_update = None;
-            self.unload(ui)
+            self.last_parse = None;
+            Ok(true)
         } else {
             // No hotkeys file, and we haven't loaded any hotkeys yet, so do nothing.
-            Ok(())
+            Ok(false)
         }
     }
 }
@@ -230,17 +264,20 @@ impl UserInterfacePlugin for InvokeHotkeysPlugin {
     }
 }
 
+#[derive(Clone, Debug)]
 struct HotkeySection {
     name: String,
     exe_filter: Option<String>,
     commands: Vec<HotkeyCommand>,
 }
 
+#[derive(Clone, Debug)]
 struct HotkeyCommand {
     name: String,
     hotkey: HotkeyCombination,
 }
 
+#[derive(Clone)]
 struct HotkeyParseResult {
     warnings: Vec<String>,
     sections: Vec<HotkeySection>,
