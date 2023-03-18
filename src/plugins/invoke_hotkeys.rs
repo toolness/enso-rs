94e51a294609275a7e08a12b591333a7c56a3c5e
Atul Varma <varmaa@gmail.com>
2023-03-18 18:07:35 -0400

    Parse hotkey sections.

--- src/plugins/invoke_hotkeys.rs
+++ src/plugins/invoke_hotkeys.rs
@@ -2,7 +2,7 @@ use std::convert::TryFrom;
 use std::path::PathBuf;
 use std::time::SystemTime;
 
-use crate::command::{Command, SimpleCommand};
+use crate::command::SimpleCommand;
 use crate::error::Error;
 use crate::system::{get_enso_home_dir, press_key, KeyDirection, VirtualKey};
 use crate::ui::{UserInterface, UserInterfacePlugin};
@@ -57,54 +57,122 @@ impl InvokeHotkeysPlugin {
         Ok(())
     }
 
-    pub fn reload(&mut self, ui: &mut UserInterface, hotkeys_path: PathBuf) -> Result<(), Error> {
-        println!("Parsing hotkeys from \"{}\".", hotkeys_path.display());
-        let mut warnings: Vec<String> = Vec::new();
-        let mut commands: Vec<Box<dyn Command>> = Vec::new();
+    fn parse_hotkeys(&self, hotkeys_path: PathBuf) -> Result<HotkeyParseResult, Error> {
         let mut line_no = 0;
+        let mut result = HotkeyParseResult {
+            warnings: vec![],
+            sections: vec![],
+        };
+        let mut current_section = HotkeySection {
+            name: "Global".to_string(),
+            exe_filter: None,
+            commands: vec![],
+        };
         for line in std::fs::read_to_string(hotkeys_path)?.lines() {
             line_no += 1;
             let line = line.trim();
             if line.is_empty() || line.starts_with('#') {
                 continue;
             }
-            let mut parts = line.rsplitn(2, ':');
-            let hotkey_str = parts.next().unwrap().trim();
-            match HotkeyCombination::try_from(hotkey_str) {
-                Ok(hotkey) => {
-                    let Some(command_name) = parts.next() else {
-                        warnings.push(format!("No colon found on line {}: {}", line_no, line));
+            if line.starts_with("@") {
+                let mut parts = line.splitn(2, ' ');
+                let directive = parts.next().unwrap().trim();
+                match directive {
+                    "@exefilter" => {
+                        if current_section.exe_filter.is_some() {
+                            result.warnings.push(format!(
+                                "Duplicate @exefilter directive on line {}",
+                                line_no
+                            ));
+                            continue;
+                        }
+                        let Some(exefilter) = parts.next() else {
+                            result.warnings.push(format!(
+                                "No value found for @exefilter on line {}",
+                                line_no
+                            ));
+                            continue;
+                        };
+                        current_section.exe_filter = Some(exefilter.trim().to_string());
+                    }
+                    "@app" => {
+                        let Some(app) = parts.next() else {
+                            result.warnings.push(format!(
+                                "No value found for @app on line {}",
+                                line_no
+                            ));
+                            continue;
+                        };
+                        result.sections.push(current_section);
+                        current_section = HotkeySection {
+                            name: app.trim().to_string(),
+                            exe_filter: None,
+                            commands: vec![],
+                        };
+                    }
+                    _ => {
+                        result.warnings.push(format!(
+                            "Unrecognized directive \"{}\" on line {}",
+                            directive, line_no
+                        ));
                         continue;
-                    };
-                    let command_name = format!("{} ({})", command_name.trim(), hotkey_str);
-                    let command =
-                        SimpleCommand::new(command_name.clone(), move |_ui| hotkey.press());
-                    commands.push(command.into_box());
+                    }
                 }
-                Err(e) => {
-                    warnings.push(format!("Error parsing hotkey on line {}: {}", line_no, e));
+            } else {
+                let mut parts = line.rsplitn(2, ':');
+                let hotkey_str = parts.next().unwrap().trim();
+                let Some(command_name) = parts.next() else {
+                    result.warnings.push(format!("No colon found on line {}: {}", line_no, line));
                     continue;
+                };
+                match HotkeyCombination::try_from(hotkey_str) {
+                    Ok(hotkey) => {
+                        let command = HotkeyCommand {
+                            name: format!("{} ({})", command_name.trim(), hotkey_str),
+                            hotkey,
+                        };
+                        current_section.commands.push(command);
+                    }
+                    Err(e) => {
+                        result
+                            .warnings
+                            .push(format!("Error parsing hotkey on line {}: {}", line_no, e));
+                        continue;
+                    }
                 }
             }
         }
+        result.sections.push(current_section);
+        Ok(result)
+    }
 
-        if warnings.len() > 0 {
+    pub fn reload(&mut self, ui: &mut UserInterface, hotkeys_path: PathBuf) -> Result<(), Error> {
+        println!("Parsing hotkeys from \"{}\".", hotkeys_path.display());
+
+        let result = self.parse_hotkeys(hotkeys_path)?;
+
+        if result.warnings.len() > 0 {
             let message = format!(
                 "Problems occurred while parsing hotkeys file:\n{}",
-                warnings.join("\n")
+                result.warnings.join("\n")
             );
             eprintln!("{}", message);
             ui.show_message(message)?;
         }
 
         self.unload(ui)?;
-        for command in commands {
-            let command_name = command.name();
-            if ui.has_command(&command_name) {
-                println!("Command \"{}\" already exists, skipping.", command_name);
-            } else {
-                self.commands_loaded.push(command_name);
-                ui.add_command(command);
+        for section in result.sections {
+            let _ = section.name; // TODO: We should use this in the help text.
+            for command in section.commands {
+                let hotkey = command.hotkey;
+                if ui.has_command(&command.name) {
+                    println!("Command \"{}\" already exists, skipping.", command.name);
+                } else {
+                    self.commands_loaded.push(command.name.clone());
+                    let simple_command =
+                        SimpleCommand::new(command.name, move |_ui| hotkey.press());
+                    ui.add_command(simple_command.into_box());
+                }
             }
         }
         println!("Loaded {} hotkey commands.", self.commands_loaded.len());
@@ -160,4 +228,20 @@ impl UserInterfacePlugin for InvokeHotkeysPlugin {
     fn on_quasimode_start(&mut self, ui: &mut UserInterface) -> Result<(), Error> {
         self.maybe_reload(ui)
     }
+}
+
+struct HotkeySection {
+    name: String,
+    exe_filter: Option<String>,
+    commands: Vec<HotkeyCommand>,
+}
+
+struct HotkeyCommand {
+    name: String,
+    hotkey: HotkeyCombination,
+}
+
+struct HotkeyParseResult {
+    warnings: Vec<String>,
+    sections: Vec<HotkeySection>,
 }
