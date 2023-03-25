ff1fc02a99685abc01ca758227101b0ae19860d4
Atul Varma <varmaa@gmail.com>
2023-03-25 09:57:22 -0400

    Prioritize hotkeys plugin over unicode plugin.

--- src/plugins.rs
+++ src/plugins.rs
@@ -7,7 +7,7 @@ mod invoke_hotkeys;
 pub fn get_all_plugins() -> Vec<Box<dyn UserInterfacePlugin>> {
     vec![
         Box::new(builtin::BuiltinPlugin::default()),
-        Box::new(insert_unicode_characters::InsertUnicodeCharactersPlugin::default()),
         Box::new(invoke_hotkeys::InvokeHotkeysPlugin::default()),
+        Box::new(insert_unicode_characters::InsertUnicodeCharactersPlugin::default()),
     ]
 }
